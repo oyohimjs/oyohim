e65d0d0d0fc701dd16d4f1c157578e91879664c4
KBLonay <sis1943@outlook.com>
2023-07-15 16:02:33 +0800

    feat(adapter): support `fetch`

--- adapter/axios/src/index.ts
+++ adapter/axios/src/index.ts
@@ -1,20 +1,28 @@
 import { Quester } from '@oyohim/quester'
-import axios, { AxiosResponse, AxiosRequestConfig } from 'axios'
+import axios, { AxiosResponse, AxiosRequestConfig, AxiosHeaders } from 'axios'
 
 declare module '@oyohim/quester' {
   interface Quester {
     axios<T = any>(config?: AxiosRequestConfig): Promise<AxiosResponse<T>>
   }
+  namespace Quester {
+  }
 }
 
-export class AxiosAdapter {
+export class AxiosAdapter implements Quester.Adapter {
   readonly name = 'axios'
 
   entity(config: Quester.Config) {
     return axios(config)
   }
 
-  quester<T = any>(method: Quester.Method, config: Quester.Config & AxiosRequestConfig) {
-    return axios.request<T>({ method, ...config })
+  async quester<T = any>(method: Quester.Method, config: Quester.Config & AxiosRequestConfig) {
+    const response = await axios.request<T>({ method, ...config })
+    return {
+      data: response.data,
+      status: response.status,
+      statusText: response.statusText,
+      headers: response.headers as unknown as Quester.Headers,
+    }
   }
 }
---
+++ adapter/fetch/readme.md
@@ -0,0 +1,20 @@
+# @oyoHiM/adapter-fetch
+
+[![npm](https://img.shields.io/npm/v/@oyohim/adapter-fetch?style=flat-square)](https://www.npmjs.com/package/@oyohim/adapter-fetch)
+![issues](https://img.shields.io/github/issues/oyohimjs/oyohim?style=flat-square)
+
+Simple http requester based on fetch.
+
+## Installation
+
+```shell
+npm install @oyohim/adapter-fetch
+
+# or use yarn
+
+yarn add @oyohim/adapter-fetch
+```
+
+## License
+
+This project is licensed under the [MIT License](./LICENSE).
